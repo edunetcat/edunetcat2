9eac0f7bdea90d415ca816907232a0eb01a32739
devilgaia <devilgaia@hotmail.com>
2015-05-01 20:12:54 +0200

    creada API pels missatges

---
+++ api/modules/v1/controllers/MissatgesController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace api\modules\v1\controllers;
+
+use yii\rest\ActiveController;
+use yii\filters\Cors;
+
+/**
+ * Centres Controller API
+ *
+ * @author Marcos
+ */
+class MissatgesController extends ActiveController {
+    public function behaviors() {
+        $behaviors = parent::behaviors ();
+        $behaviors ['corsFilter'] = [ 
+                'class' => Cors::className (),
+                'cors' => [ 
+                        'Origin' => [ 
+                                '*' 
+                        ],
+                        // 'Access-Control-Request-Method' => ['*'],
+                        'Access-Control-Request-Method' => [ 
+                                'POST',
+                                'GET',
+                                'PUT',
+                                'DELETE',
+                                'HEADER',
+                                'OPTIONS' 
+                        ],
+                        'Access-Control-Request-Headers' => [ 
+                                '*' 
+                        ] 
+                ] 
+        ]
+        // 'Access-Control-Request-Headers' => ['Expiry'],
+        
+        ;
+        
+        return $behaviors;
+    }
+    public $modelClass = 'api\modules\v1\models\Missatges';
+}
+
+
---
+++ api/modules/v1/models/Missatges.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace api\modules\v1\models;
+
+use \yii\db\ActiveRecord;
+
+/**
+ * Model centres
+ *
+ * @author Marcos
+ */
+class Missatges extends ActiveRecord {
+    /**
+     * @inheritdoc
+     */
+    public static function tableName() {
+        return 'missatges';
+    }
+    
+    /**
+     * defineix les regles de validació
+     */
+    public function rules() {
+        return [ 
+                [ 
+                        [ 
+                                'nom' 
+                        ],
+                        'required' 
+                ],
+                [ 
+                        [ 
+                                'direccio',
+                                'telefon',
+                                'poblacio' 
+                        ],
+                        'default' 
+                ] 
+        ];
+    }
+}
