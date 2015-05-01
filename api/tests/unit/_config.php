f6b0657fcbbdec7c2674a1cbdbc4bc5916511cae
devilgaia <devilgaia@hotmail.com>
2015-05-01 20:05:30 +0200

    Arreglades configuracions del codeception (2)

--- api/tests/unit/_config.php
+++ api/tests/unit/_config.php
@@ -2,7 +2,11 @@
 return yii\helpers\ArrayHelper::merge ( require (__DIR__ . '/../../../common/config/main.php'), require (__DIR__ . '/../../../common/config/main-local.php'), require (__DIR__ . '/../../config/main.php'), require (__DIR__ . '/../../config/main-local.php'), require (__DIR__ . '/../_config.php'), [ 
 		'components' => [ 
 				'db' => [ 
-						'dsn' => 'mysql:host=localhost;dbname=edunetcat' 
+						'class' => 'yii\db\Connection',
+						'dsn' => 'mysql:host=localhost;dbname=edunetcat',
+						'username' => 'root',
+						'password' => '',
+						'charset' => 'utf8' 
 				] 
 		] 
 ] );
