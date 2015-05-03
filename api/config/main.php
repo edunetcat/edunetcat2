e61a207cce888071675823f6e4343042c0c42245
devilgaia <devilgaia@hotmail.com>
2015-05-03 19:08:50 +0200

    Arreglat fallo que feia que la portada no funciones
    afegit cookieValidationKey

--- api/config/main.php
+++ api/config/main.php
@@ -2,87 +2,87 @@
 $params = array_merge ( require (__DIR__ . '/../../common/config/params.php'), require (__DIR__ . '/../../common/config/params-local.php'), require (__DIR__ . '/params.php'), require (__DIR__ . '/params-local.php') );
 
 return [ 
-		'id' => 'app-api',
-		'basePath' => dirname ( __DIR__ ),
-		'bootstrap' => [ 
-				'log' 
-		],
-		'modules' => [ 
-				'v1' => [ 
-						'basePath' => '@app/modules/v1',
-						'class' => 'api\modules\v1\Module' 
-				] 
-		],
-		'components' => [ 
-				'request' => [
-					'class' => '\yii\web\Request',
-					'parsers' => [
-						'application/json' => 'yii\web\JsonParser'
-					]
-				],
-				'user' => [ 
-						'identityClass' => 'common\models\User',
-						'enableAutoLogin' => false 
-				],
-				'log' => [ 
-						'traceLevel' => YII_DEBUG ? 3 : 0,
-						'targets' => [ 
-								[ 
-										'class' => 'yii\log\FileTarget',
-										'levels' => [ 
-												'error',
-												'warning' 
-										] 
-								] 
-						] 
-				],
-				'urlManager' => [ 
-						'enablePrettyUrl' => true,
-						'enableStrictParsing' => true,
-						'showScriptName' => false,
-						'rules' => [ 
-								[ 
-										'class' => 'yii\rest\UrlRule',
-										'controller' => 'v1/centres',
-										'tokens' => [ 
-												'{id}' => '<id:\\w+>' 
-										] 
-								],
-								[ 
-										'class' => 'yii\rest\UrlRule',
-										'controller' => 'v1/missatges',
-										'tokens' => [ 
-												'{id}' => '<id:\\w+>' 
-										] 
-								],
-								[ 
-										'class' => 'yii\rest\UrlRule',
-										'controller' => 'v1/cursos',
-										'tokens' => [ 
-												'{id}' => '<id:\\w+>' 
-										] 
-								],
-								[ 
-										'class' => 'yii\rest\UrlRule',
-										'controller' => 'v1/assignatures',
-										'tokens' => [ 
-												'{id}' => '<id:\\w+>' 
-										] 
-								],
-								[ 
-										'class' => 'yii\rest\UrlRule',
-										'controller' => 'v1/persona',
-										'tokens' => [ 
-												'{id}' => '<id:\\w+>' 
-										] 
-								],
-								'v1/login/<user>/<password>' => 'v1/login/login',
-								'v1/lamevainfo/<key>' => '/v1/persones/lamevainfo' 
-						] 
-				]
-				 
-		],
-		'params' => $params, 
+        'id' => 'app-api',
+        'basePath' => dirname ( __DIR__ ),
+        'bootstrap' => [ 
+                'log' 
+        ],
+        'modules' => [ 
+                'v1' => [ 
+                        'basePath' => '@app/modules/v1',
+                        'class' => 'api\modules\v1\Module' 
+                ] 
+        ],
+        'components' => [ 
+                'request' => [ 
+                        
+                        'class' => '\yii\web\Request',
+                        'parsers' => [ 
+                                'application/json' => 'yii\web\JsonParser' 
+                        ] 
+                ],
+                'user' => [ 
+                        'identityClass' => 'common\models\User',
+                        'enableAutoLogin' => false 
+                ],
+                'log' => [ 
+                        'traceLevel' => YII_DEBUG ? 3 : 0,
+                        'targets' => [ 
+                                [ 
+                                        'class' => 'yii\log\FileTarget',
+                                        'levels' => [ 
+                                                'error',
+                                                'warning' 
+                                        ] 
+                                ] 
+                        ] 
+                ],
+                'urlManager' => [ 
+                        'enablePrettyUrl' => true,
+                        'enableStrictParsing' => true,
+                        'showScriptName' => false,
+                        'rules' => [ 
+                                [ 
+                                        'class' => 'yii\rest\UrlRule',
+                                        'controller' => 'v1/centres',
+                                        'tokens' => [ 
+                                                '{id}' => '<id:\\w+>' 
+                                        ] 
+                                ],
+                                [ 
+                                        'class' => 'yii\rest\UrlRule',
+                                        'controller' => 'v1/missatges',
+                                        'tokens' => [ 
+                                                '{id}' => '<id:\\w+>' 
+                                        ] 
+                                ],
+                                [ 
+                                        'class' => 'yii\rest\UrlRule',
+                                        'controller' => 'v1/cursos',
+                                        'tokens' => [ 
+                                                '{id}' => '<id:\\w+>' 
+                                        ] 
+                                ],
+                                [ 
+                                        'class' => 'yii\rest\UrlRule',
+                                        'controller' => 'v1/assignatures',
+                                        'tokens' => [ 
+                                                '{id}' => '<id:\\w+>' 
+                                        ] 
+                                ],
+                                [ 
+                                        'class' => 'yii\rest\UrlRule',
+                                        'controller' => 'v1/persona',
+                                        'tokens' => [ 
+                                                '{id}' => '<id:\\w+>' 
+                                        ] 
+                                ],
+                                'v1/login/<user>/<password>' => 'v1/login/login',
+                                'v1/lamevainfo/<key>' => '/v1/persones/lamevainfo' 
+                        ] 
+                ] 
+        ],
+        'params' => $params 
 ];
 
 
--- common/config/main.php
+++ common/config/main.php
@@ -1,9 +1,14 @@
 <?php
-return [
-    'vendorPath' => dirname(dirname(__DIR__)) . '/vendor',
-    'components' => [
-        'cache' => [
-            'class' => 'yii\caching\FileCache',
-        ],
-    ],
+return [ 
+        'vendorPath' => dirname ( dirname ( __DIR__ ) ) . '/vendor',
+        'components' => [ 
+                'cache' => [ 
+                        'class' => 'yii\caching\FileCache' 
+                ],
+                'request' => [ 
+                        'enableCookieValidation' => true,
+                        'enableCsrfValidation' => true,
+                        'cookieValidationKey' => 'xxxxxxx' 
+                ] 
+        ] 
 ];
