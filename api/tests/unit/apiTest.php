b54ae94a0c637498c5745c36052dfd0d693e009b
devilgaia <devilgaia@hotmail.com>
2015-05-07 17:38:14 +0200

    Tests unitaris a centres, missatges i cursos
    afegit un test.bat que automatitza la acció d'executar:
    cls
    codecept run --colors

---
+++ api/tests/unit/apiTest.php
@@ -0,0 +1,52 @@
+<?php
+use api\modules\v1\models\Centres;
+require_once (__DIR__ . '/Funciones.php'); // funcio per llegir la api remotament
+require_once (__DIR__ . '/_constants.php'); // per indicar la direccio de la api
+class apiTest extends \Codeception\TestCase\Test {
+    /**
+     *
+     * @var \UnitTester
+     * @author Marcos Torrent
+     */
+    protected $tester;
+    protected function _before() {
+    }
+    protected function _after() {
+    }
+    
+    // tests
+    
+    /**
+     * En aquest test agafem la id del centre numero 1, per tant el resultat haurà de ser igualment 1
+     */
+    public function testCentre1() {
+        
+        /*
+         * get_remote_data ens retorna un array de 2 posicions.
+         * El primer element del array conté el codi json per tant agafem el resultat de la primera posició del array i
+         * el descodifiquem amb json_decode, i això serà el resultat convertit en un objecte php
+         */
+        $resultat = json_decode ( Funciones::get_remote_data ( URLAPIEDUNET . '/v1/centres/1' )[0] );
+        
+        // amb aquest mètode comprovem que la peticio a la api i el funcionament de la api buscant centres és correcte
+        $this->assertEquals ( $resultat->id, 1 );
+    }
+    /**
+     * En aquest test mirem si la resposta dels missatge numero 0 (que no existeix, es un array amb el missatge 404
+     */
+    public function testMissatge0() {
+        $resultat = json_decode ( Funciones::get_remote_data ( URLAPIEDUNET . '/v1/missatges/0' )[0] );
+        
+        $this->assertEquals ( $resultat->status, 404 );
+    }
+    
+    /**
+     * En aquest test mirem si la resposta es un array amb més de un curs
+     */
+    public function testCursos() {
+        $resultat = json_decode ( Funciones::get_remote_data ( URLAPIEDUNET . '/v1/cursos' )[0] );
+        
+        $this->assertTrue ( sizeof ( $resultat ) > 1 );
+    }
+}
+
