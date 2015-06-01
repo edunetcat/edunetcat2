4b79e0de610ba94ada99e4640978259618f3e7cf
devilgaia <devilgaia@hotmail.com>
2015-06-01 11:09:27 +0200

    ecpedient i avaluacio

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -8,189 +8,150 @@ use frontend\assets\AppAsset;
 /* @var $this \yii\web\View */
 /* @var $content string */
 
-AppAsset::register($this);
+AppAsset::register ( $this );
 ?>
-<?php $this->beginPage() ?>
+<?php $this->beginPage()?>
 <!DOCTYPE html>
 <html lang="<?= Yii::$app->language ?>">
 <head>
-    <meta charset="<?= Yii::$app->charset ?>">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="description" content="">
-    <meta name="author" content="">
+<meta charset="<?= Yii::$app->charset ?>">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="description" content="">
+<meta name="author" content="">
 
-    <title><?= Html::encode($this->title) ?></title>
-    <?php $this->head() ?>
+<title><?= Html::encode($this->title) ?></title>
+    <?php $this->head()?>
 
     <!-- Custom CSS -->
-    <link href="css/panell.css" rel="stylesheet">
-    
-    <!-- Custom Fonts -->
-    <link href="css/font-awesome.min.css" rel="stylesheet" type="text/css">
-    <link href="css/angular-bootstrap-calendar.css" rel="stylesheet" type="text/css">
+<link href="css/panell.css" rel="stylesheet">
+
+<!-- Custom Fonts -->
+<link href="css/font-awesome.min.css" rel="stylesheet" type="text/css">
+<link href="css/angular-bootstrap-calendar.css" rel="stylesheet" type="text/css">
 
 </head>
 <body ng-app="panellApp">
 
 
 
-<?php $this->beginBody() ?>
+<?php $this->beginBody()?>
     <div id="wrapper">
 
-        <!-- Navigation -->
-        <nav class="navbar navbar-default navbar-static-top" role="navigation" style="margin-bottom: 0">
-            <div class="navbar-header">
-                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-                    <span class="sr-only">Toggle navigation</span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </button>
-                <a class="navbar-brand" href="">Edunet.cat - Panell Alumnes</a>
-            </div>
-            <!-- /.navbar-header -->
-            
-            
+		<!-- Navigation -->
+		<nav class="navbar navbar-default navbar-static-top" role="navigation" style="margin-bottom: 0">
+			<div class="navbar-header">
+				<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+					<span class="sr-only">Toggle navigation</span> <span class="icon-bar"></span> <span class="icon-bar"></span> <span
+						class="icon-bar"></span>
+				</button>
+				<a class="navbar-brand" href="">Edunet.cat - Panell Alumnes</a>
+			</div>
+			<!-- /.navbar-header -->
 
-            <ul class="nav navbar-top-links navbar-right">
-                <!--<li>
+
+
+			<ul class="nav navbar-top-links navbar-right">
+				<!--<li>
                     <a>AuthKey: <?php $session = Yii::$app->session; echo $session['authKey'] ?></a>
                 </li> -->
-                <li class="dropdown">
-                    <a class="dropdown-toggle" data-toggle="dropdown" href="">
-                        <!-- <span class="badge important" style="background-color: #d9534f;">1</span>-->
-                        <i class="fa fa-envelope fa-fw"></i> Missatges <i class="fa fa-caret-down"></i>
-                    </a>
-                    <ul class="dropdown-menu dropdown-messages">
-                        <li>
-                            <a href="/#/missatges-read">
-                                <div>
-                                    <strong>Professor sistemes</strong>
-                                    <span class="pull-right text-muted">
-                                        <em>09:10</em>
-                                    </span>
-                                </div>
-                                <div>RE: Dubtes amb ex3 EAC4?</div>
-                            </a>
-                        </li>
-                        <li class="divider"></li>
-                        <li>
-                            <a class="text-center" href="/#/missatges">
-                                <strong>Llegir la resta de missatges</strong>
-                                <i class="fa fa-angle-right"></i>
-                            </a>
-                        </li>
-                    </ul>
-                    <!-- /.dropdown-messages -->
-                </li>
-                
-                
-                <!-- /.dropdown -->
-                <li class="dropdown">
-                    <a class="dropdown-toggle" data-toggle="dropdown" href="">
-                        <i class="fa fa-user fa-fw"></i> Biel Conde <i class="fa fa-caret-down"></i>
-                    </a>
-                    <ul class="dropdown-menu dropdown-user">
-                        <li><a href="/#/perfil"><i class="fa fa-user fa-fw"></i> Perfil</a>
-                        </li>
-                        <li class="divider"></li>
-                        <li><a href="#/logout"><i class="fa fa-sign-out fa-fw"></i> Logout</a>
-                        </li>
-                    </ul>
-                    <!-- /.dropdown-user -->
-                </li>
-                <!-- /.dropdown -->
-            </ul>
-            <!-- /.navbar-top-links -->
+				<li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" href=""> <!-- <span class="badge important" style="background-color: #d9534f;">1</span>-->
+						<i class="fa fa-envelope fa-fw"></i> Missatges <i class="fa fa-caret-down"></i>
+				</a>
+					<ul class="dropdown-menu dropdown-messages">
+						<li><a href="/#/missatges-read">
+								<div>
+									<strong>Professor sistemes</strong> <span class="pull-right text-muted"> <em>09:10</em>
+									</span>
+								</div>
+								<div>RE: Dubtes amb ex3 EAC4?</div>
+						</a></li>
+						<li class="divider"></li>
+						<li><a class="text-center" href="/#/missatges"> <strong>Llegir la resta de missatges</strong> <i
+								class="fa fa-angle-right"></i>
+						</a></li>
+					</ul> <!-- /.dropdown-messages --></li>
 
-            <div class="navbar-default sidebar" role="navigation">
-                <div class="sidebar-nav navbar-collapse">
-                    <ul class="nav" id="side-menu">
-                        <li class="sidebar-search">
-                            <div class="input-group custom-search-form">
-                                <input type="text" class="form-control" placeholder="Búsqueda...">
-                                <span class="input-group-btn">
-                                <button class="btn btn-default" type="button">
-                                    <i class="fa fa-search"></i>
-                                </button>
-                            </span>
-                            </div>
-                            <!-- /input-group -->
-                        </li>
-                        <li>
-                            <a href="/#/"><i class="fa fa-dashboard fa-fw"></i> Panell Alumnes</a>
-                        </li>
-                        <!-- <li><a href="/#/centres"><i class="fa fa-building fa-fw"></i> Administrar Centres</a></li>-->
-                        <!-- <li><a href="/#/usuaris"><i class="fa fa-building fa-fw"></i> Administrar Usuaris</a></li> -->
-                        <li>
-                            <a href="/#/tramits"><i class="fa fa-sign-in fa-fw"></i> Tràmits</a>
-                        </li>
-                        <li>
-                            <a href="/#/missatges"><i class="fa fa-envelope-o fa-fw"></i> Missatges</a>
-                        </li>
-                        <li>
-                            <a href="/#/calendari"><i class="fa fa-calendar-o fa-fw"></i> Calendari</a>
-                        </li>
-                        <li>
-                            <a href="/#/panell"><i class="fa fa-bar-chart-o fa-fw"></i> Expedient acadèmic</a>
-                        </li>
-                        <li>
-                            <a href="/#/panell"><i class="fa fa-edit fa-fw"></i> Avaluació <span class="fa arrow"></span></a>
-                            <ul class="nav nav-second-level">
-                                <li>
-                                    <a href="/#/panell">M02 - Base de Dades</a>
-                                </li>
-                                <li>
-                                    <a href="/#/panell">M03 - Sistemes</a>
-                                </li>
-                                <li>
-                                    <a href="/#/panell">M06 - Accés a dades</a>
-                                </li>
-                            </ul>
-                            <!-- /.nav-second-level -->
-                        </li>
-                    </ul>
-                </div>
-                <!-- /.sidebar-collapse -->
-            </div>
-            <!-- /.navbar-static-side -->
-        </nav>       
-        
 
-        <?= $content ?>
+				<!-- /.dropdown -->
+				<li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" href=""> <i class="fa fa-user fa-fw"></i>
+						Biel Conde <i class="fa fa-caret-down"></i>
+				</a>
+					<ul class="dropdown-menu dropdown-user">
+						<li><a href="/#/perfil"><i class="fa fa-user fa-fw"></i> Perfil</a></li>
+						<li class="divider"></li>
+						<li><a href="#/logout"><i class="fa fa-sign-out fa-fw"></i> Logout</a></li>
+					</ul> <!-- /.dropdown-user --></li>
+				<!-- /.dropdown -->
+			</ul>
+			<!-- /.navbar-top-links -->
+
+			<div class="navbar-default sidebar" role="navigation">
+				<div class="sidebar-nav navbar-collapse">
+					<ul class="nav" id="side-menu">
+						<li class="sidebar-search">
+							<div class="input-group custom-search-form">
+								<input type="text" class="form-control" placeholder="Búsqueda..."> <span class="input-group-btn">
+									<button class="btn btn-default" type="button">
+										<i class="fa fa-search"></i>
+									</button>
+								</span>
+							</div> <!-- /input-group -->
+						</li>
+						<li><a href="/#/"><i class="fa fa-dashboard fa-fw"></i> Panell Alumnes</a></li>
+						<!-- <li><a href="/#/centres"><i class="fa fa-building fa-fw"></i> Administrar Centres</a></li>-->
+						<!-- <li><a href="/#/usuaris"><i class="fa fa-building fa-fw"></i> Administrar Usuaris</a></li> -->
+						<li><a href="/#/tramits"><i class="fa fa-sign-in fa-fw"></i> Tràmits</a></li>
+						<li><a href="/#/missatges"><i class="fa fa-envelope-o fa-fw"></i> Missatges</a></li>
+						<li><a href="/#/calendari"><i class="fa fa-calendar-o fa-fw"></i> Calendari</a></li>
+						<li><a href="/#/expedient"><i class="fa fa-bar-chart-o fa-fw"></i> Expedient acadèmic</a></li>
+						<li><a href="/#/panell"><i class="fa fa-edit fa-fw"></i> Avaluació <span class="fa arrow"></span></a>
+							<ul class="nav nav-second-level">
+								<li><a href="/#/avaluacio-dam-m02">M02 - Base de Dades</a></li>
+								<li><a href="/#/avaluacio-dam-m03">M03 - Sistemes</a></li>
+								<li><a href="/#/avaluacio-dam-m06">M06 - Accés a dades</a></li>
+							</ul> <!-- /.nav-second-level --></li>
+					</ul>
+				</div>
+				<!-- /.sidebar-collapse -->
+			</div>
+			<!-- /.navbar-static-side -->
+		</nav>
+
+
+        <?= $content?>
 
     </div>
-    <!-- /wrapper -->
-    
-    <!-- jQuery -->
-    <script src="/js/vendor/jquery-1.11.1.min.js"></script>
-    
-    <!-- Bootstrap Core JavaScript -->
-    <script src="/js/vendor/bootstrap.min.js"></script>
-    
-    <!-- Metis Menu Plugin JavaScript -->
-    <script src="/js/vendor/metisMenu.min.js"></script>
+	<!-- /wrapper -->
 
-    <!-- angular 1.4.0-rc.0 -->
-    <script src="/js/vendor/angular.min.js"></script>
-    <script src="/js/vendor/angular-route.min.js"></script>
-    <script src="/js/vendor/angular-resource.min.js"></script>
+	<!-- jQuery -->
+	<script src="/js/vendor/jquery-1.11.1.min.js"></script>
 
-    <script src="/js/vendor/ui-bootstrap-0.13.0.min.js"></script>
-    <script src="/js/vendor/moment.min.js"></script>
-    <script src="/js/vendor/moment-ca.js"></script>
-    <script src="/js/vendor/ui-bootstrap-tpls.min.js"></script>
-    <script src="/js/vendor/angular-bootstrap-calendar-tpls.min.js"></script>
+	<!-- Bootstrap Core JavaScript -->
+	<script src="/js/vendor/bootstrap.min.js"></script>
 
-    <!-- Custom JavaScript -->
-    <script src="/js/panellapp.js"></script>
-    <script src="/js/controllers.js"></script>
-    <script src="/js/services.js"></script>
+	<!-- Metis Menu Plugin JavaScript -->
+	<script src="/js/vendor/metisMenu.min.js"></script>
 
-    
+	<!-- angular 1.4.0-rc.0 -->
+	<script src="/js/vendor/angular.min.js"></script>
+	<script src="/js/vendor/angular-route.min.js"></script>
+	<script src="/js/vendor/angular-resource.min.js"></script>
 
-<?php $this->endBody() ?>
+	<script src="/js/vendor/ui-bootstrap-0.13.0.min.js"></script>
+	<script src="/js/vendor/moment.min.js"></script>
+	<script src="/js/vendor/moment-ca.js"></script>
+	<script src="/js/vendor/ui-bootstrap-tpls.min.js"></script>
+	<script src="/js/vendor/angular-bootstrap-calendar-tpls.min.js"></script>
+
+	<!-- Custom JavaScript -->
+	<script src="/js/panellapp.js"></script>
+	<script src="/js/controllers.js"></script>
+	<script src="/js/services.js"></script>
+
+
+
+<?php $this->endBody()?>
 </body>
 </html>
 <?php $this->endPage() ?>
--- frontend/web/js/panellapp.js
+++ frontend/web/js/panellapp.js
@@ -1,171 +1,170 @@
- 'use strict';
+'use strict';
 
 /**
-*   Fitxer principal de l'aplicació Angular.js. Aquí es defineix 
-*   l'aplicació "panellApp" i les rutes de tota la SPA (single page aplication). 
-*   
-*   @author: Biel <bielbcm@gmail.com>
-* 
-**/
+ * Fitxer principal de l'aplicació Angular.js. Aquí es defineix l'aplicació
+ * "panellApp" i les rutes de tota la SPA (single page aplication).
+ * 
+ * @author: Biel <bielbcm@gmail.com>
+ * 
+ */
 var panellApp = angular.module('panellApp', [
-    // angular modules
-    'ngRoute', 
-    'ngResource', 
-    'mwl.calendar', 
-    'ui.bootstrap',
-    // controllers
-    'panellAppControllers', 
-    //services
-    'panellAppServices'
-]);
-
+// angular modules
+'ngRoute', 'ngResource', 'mwl.calendar', 'ui.bootstrap',
+// controllers
+'panellAppControllers',
+// services
+'panellAppServices' ]);
 
 /**
-*   Aquí estan definides les rutes de la SPA. Per cada regla, es defineix el 
-*   template html i el controller encarregat de gestionar la página. 
-*
-*   @author: Biel <bielbcm@gmail.com>
-*/
-panellApp.config(['$routeProvider', '$httpProvider',
-    function($routeProvider, $httpProvider) {
+ * Aquí estan definides les rutes de la SPA. Per cada regla, es defineix el
+ * template html i el controller encarregat de gestionar la página.
+ * 
+ * @author: Biel <bielbcm@gmail.com>
+ */
+panellApp.config([ '$routeProvider', '$httpProvider',
+		function($routeProvider, $httpProvider) {
 
-        $routeProvider
-        .when('/panell', {
-            templateUrl: 'ng-views/panell.html',
-            controller: 'PanellCtrl'
-        })
-        //vistes administració de centres
-        .when('/centres', {
-            templateUrl: 'ng-views/centres.html',
-            controller: 'CentresListCtrl'
-        })
-        .when('/centres-add', {
-            templateUrl: 'ng-views/centres-add.html',
-            controller: 'CentresAddCtrl'
-        })
-        .when('/centres-edit/:id', {
-            templateUrl: 'ng-views/centres-edit.html',
-            controller: 'CentresEditCtrl'
-        })
-        //vistes administració d'usuaris
-        .when('/usuaris', {
-            templateUrl: 'ng-views/usuaris.html',
-            controller: 'UsuarisListCtrl'
-        })
-        .when('/usuaris-add', {
-            templateUrl: 'ng-views/usuaris-add.html',
-            controller: 'UsuarisAddCtrl'
-        })
-        .when('/usuaris-edit', {
-            templateUrl: 'ng-views/usuaris-edit.html',
-            controller: 'UsuarisEditCtrl'
-        })
-        // missatges
-        .when('/missatges', {
-            templateUrl: 'ng-views/missatges.html',
-            controller: 'MissatgesListCtrl'
-        })
-        .when('/missatges-professorat', {
-            templateUrl: 'ng-views/missatges-professorat.html',
-            controller: 'MissatgesListCtrl'
-        })
-        .when('/missatges-alumnat', {
-            templateUrl: 'ng-views/missatges-alumnat.html',
-            controller: 'MissatgesListCtrl'
-        })
-        .when('/missatges-llegits', {
-            templateUrl: 'ng-views/missatges-llegits.html',
-            controller: 'MissatgesListCtrl'
-        })
-        .when('/missatges-enviats', {
-            templateUrl: 'ng-views/missatges-enviats.html',
-            controller: 'MissatgesListCtrl'
-        })
-        .when('/missatges-new', {
-            templateUrl: 'ng-views/missatges-new.html',
-            controller: 'MissatgesListCtrl'
-        })
-        .when('/missatges-read', {
-            templateUrl: 'ng-views/missatges-read.html',
-            controller: 'MissatgesListCtrl'
-        })
-        .when('/calendari', {
-            templateUrl: 'ng-views/calendari.html',
-            controller: 'CalendariCtrl'
-        })
-        .when('/tramits', {
-            templateUrl: 'ng-views/tramits.html',
-            controller: 'TramitsCtrl'
-        })
-        .when('/tramit-alegacions', {
-            templateUrl: 'ng-views/tramit-alegacions.html',
-            controller: 'TramitsCtrl'
-        })
-        .when('/tramit-alegacions-88', {
-            templateUrl: 'ng-views/tramit-alegacions-88.html',
-            controller: 'TramitsCtrl'
-        })
-         .when('/tramit-anulacions', {
-            templateUrl: 'ng-views/tramit-anulacions.html',
-            controller: 'TramitsCtrl'
-        })
-        .when('/perfil', {
-            templateUrl: 'ng-views/perfil.html',
-            controller: 'PerfilCtrl'
-        })
+			$routeProvider.when('/panell', {
+				templateUrl : 'ng-views/panell.html',
+				controller : 'PanellCtrl'
+			})
+			// vistes administració de centres
+			.when('/centres', {
+				templateUrl : 'ng-views/centres.html',
+				controller : 'CentresListCtrl'
+			}).when('/centres-add', {
+				templateUrl : 'ng-views/centres-add.html',
+				controller : 'CentresAddCtrl'
+			}).when('/centres-edit/:id', {
+				templateUrl : 'ng-views/centres-edit.html',
+				controller : 'CentresEditCtrl'
+			})
+			// vistes administració d'usuaris
+			.when('/usuaris', {
+				templateUrl : 'ng-views/usuaris.html',
+				controller : 'UsuarisListCtrl'
+			}).when('/usuaris-add', {
+				templateUrl : 'ng-views/usuaris-add.html',
+				controller : 'UsuarisAddCtrl'
+			}).when('/usuaris-edit', {
+				templateUrl : 'ng-views/usuaris-edit.html',
+				controller : 'UsuarisEditCtrl'
+			})
+			// missatges
+			.when('/missatges', {
+				templateUrl : 'ng-views/missatges.html',
+				controller : 'MissatgesListCtrl'
+			}).when('/missatges-professorat', {
+				templateUrl : 'ng-views/missatges-professorat.html',
+				controller : 'MissatgesListCtrl'
+			}).when('/missatges-alumnat', {
+				templateUrl : 'ng-views/missatges-alumnat.html',
+				controller : 'MissatgesListCtrl'
+			}).when('/missatges-llegits', {
+				templateUrl : 'ng-views/missatges-llegits.html',
+				controller : 'MissatgesListCtrl'
+			}).when('/missatges-enviats', {
+				templateUrl : 'ng-views/missatges-enviats.html',
+				controller : 'MissatgesListCtrl'
+			}).when('/missatges-new', {
+				templateUrl : 'ng-views/missatges-new.html',
+				controller : 'MissatgesListCtrl'
+			}).when('/missatges-read', {
+				templateUrl : 'ng-views/missatges-read.html',
+				controller : 'MissatgesListCtrl'
+			}).when('/calendari', {
+				templateUrl : 'ng-views/calendari.html',
+				controller : 'CalendariCtrl'
+			}).when('/tramits', {
+				templateUrl : 'ng-views/tramits.html',
+				controller : 'TramitsCtrl'
+			}).when('/tramit-alegacions', {
+				templateUrl : 'ng-views/tramit-alegacions.html',
+				controller : 'TramitsCtrl'
+			}).when('/tramit-alegacions-88', {
+				templateUrl : 'ng-views/tramit-alegacions-88.html',
+				controller : 'TramitsCtrl'
+			}).when('/tramit-anulacions', {
+				templateUrl : 'ng-views/tramit-anulacions.html',
+				controller : 'TramitsCtrl'
+			}).when('/perfil', {
+				templateUrl : 'ng-views/perfil.html',
+				controller : 'PerfilCtrl'
+			})
 
-        .when('/logout', {
-            templateUrl: 'ng-views/panell.html',
-            controller: 'LogoutCtrl'
-        })        
-        .otherwise({
-            redirectTo: '/panell'
-        });
-    }]);
+			.when('/expedient', {
+				templateUrl : 'ng-views/expedient.html',
+				controller : 'PanellCtrl'
+			})
 
+			.when('/avaluacio-dam-m02', {
+				templateUrl : 'ng-views/avaluacio-dam-m02.html',
+				controller : 'PanellCtrl'
+			})
 
+			.when('/avaluacio-dam-m03', {
+				templateUrl : 'ng-views/avaluacio-dam-m03.html',
+				controller : 'PanellCtrl'
+			})
+
+			.when('/avaluacio-dam-m06', {
+				templateUrl : 'ng-views/avaluacio-dam-m06.html',
+				controller : 'PanellCtrl'
+			})
+
+			.when('/logout', {
+				templateUrl : 'ng-views/panell.html',
+				controller : 'LogoutCtrl'
+			}).otherwise({
+				redirectTo : '/panell'
+			});
+		} ]);
 
 /**
-*   TODO: solucionar .metisMenu();
-*
-*   @author: Biel <bielbcm@gmail.com>
-*/
+ * TODO: solucionar .metisMenu();
+ * 
+ * @author: Biel <bielbcm@gmail.com>
+ */
 $(function() {
-    $('#side-menu').metisMenu();
+	$('#side-menu').metisMenu();
 });
 
-
 /**
-*   Carrega la sidebar on window load. Alhora, fa collapse 
-*   del menu quan es cambia el tamany de la finestra. 
-*
-*   @author: Biel <bielbcm@gmail.com>
-*/
+ * Carrega la sidebar on window load. Alhora, fa collapse del menu quan es
+ * cambia el tamany de la finestra.
+ * 
+ * @author: Biel <bielbcm@gmail.com>
+ */
 $(function() {
-    $(window).bind("load resize", function() {
-        var topOffset = 50;
-        var width = (this.window.innerWidth > 0) ? this.window.innerWidth : this.screen.width;
-        if (width < 768) {
-            $('div.navbar-collapse').addClass('collapse');
-            topOffset = 100; // 2-row-menu
-        } else {
-            $('div.navbar-collapse').removeClass('collapse');
-        }
+	$(window)
+			.bind(
+					"load resize",
+					function() {
+						var topOffset = 50;
+						var width = (this.window.innerWidth > 0) ? this.window.innerWidth
+								: this.screen.width;
+						if (width < 768) {
+							$('div.navbar-collapse').addClass('collapse');
+							topOffset = 100; // 2-row-menu
+						} else {
+							$('div.navbar-collapse').removeClass('collapse');
+						}
 
-        var height = ((this.window.innerHeight > 0) ? this.window.innerHeight : this.screen.height) - 1;
-        height = height - topOffset;
-        if (height < 1) height = 1;
-        if (height > topOffset) {
-            $("#page-wrapper").css("min-height", (height) + "px");
-        }
-    });
+						var height = ((this.window.innerHeight > 0) ? this.window.innerHeight
+								: this.screen.height) - 1;
+						height = height - topOffset;
+						if (height < 1)
+							height = 1;
+						if (height > topOffset) {
+							$("#page-wrapper").css("min-height",
+									(height) + "px");
+						}
+					});
 
-    /*
-    var url = window.location;
-    var element = $('ul.nav a').filter(function() {
-        return this.href == url || url.href.indexOf(this.href) == 0;
-    }).addClass('active').parent().parent().addClass('in').parent();
-    if (element.is('li')) {
-        element.addClass('active');
-    }*/
+	/*
+	 * var url = window.location; var element = $('ul.nav a').filter(function() {
+	 * return this.href == url || url.href.indexOf(this.href) == 0;
+	 * }).addClass('active').parent().parent().addClass('in').parent(); if
+	 * (element.is('li')) { element.addClass('active'); }
+	 */
 });
